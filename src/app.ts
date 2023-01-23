34c1f4cb9ea3049e18ff03c4762e07bff02a7b13
gustavo-de-assis <gustavo.siqueira.421@gmail.com>
2023-01-23 11:52:37 -0300

    feat: create database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import productRoute from "./routes/products.route.js";
 
 const app = express();
 
 app
     .use(cors())
     .use(express.json())
-
+    .get('/health', (req, res)=>{ res.send('OK')})
+    .use(productRoute)
 
 
 app.listen(4000, ()=>{
---
+++ src/routes/products.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const productRoute = Router();
+
+/*
+productRoute.get("/products", listProducts);
+productRoute.get("/products/:id", productInfo);
+productRoute.post("/register", insertProduct);
+productRoute.delete("/remove/:id");
+productRoute.update("/update/:id");
+*/
+export default productRoute;
